1cf5f39a759c8b6556c9b9648be903074f89c064
maybe-dog <sa990130@gmail.com>
2023-12-18 02:37:00 +0900

    feat: tenantDBに対するINDEXを冪等にした

--- sql/tenant/11_index.sql
+++ sql/tenant/11_index.sql
@@ -1,3 +1,9 @@
-CREATE INDEX competition_idx ON competition(tenant_id);
-CREATE INDEX player_idx ON player(tenant_id);
-CREATE INDEX player_score_idx ON player_score(tenant_id, player_id, competition_id);
+CREATE INDEX IF NOT EXISTS competition_idx ON competition(tenant_id);
+
+CREATE INDEX IF NOT EXISTS player_idx ON player(tenant_id);
+
+CREATE INDEX IF NOT EXISTS player_score_idx ON player_score(
+    tenant_id,
+    player_id,
+    competition_id
+);
