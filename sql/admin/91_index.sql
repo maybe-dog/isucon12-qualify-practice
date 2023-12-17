71168a61e5c76e1e1fe8c1b7740cd7ce9a15b5a4
maybe-dog <sa990130@gmail.com>
2023-12-18 01:43:49 +0900

    feat: covering indexに変更

--- sql/admin/91_index.sql
+++ sql/admin/91_index.sql
@@ -1,1 +1,7 @@
-ALTER TABLE `visit_history` ADD INDEX `visit_history_multi_index` (`tenant_id`, `competition_id`);
+ALTER TABLE `visit_history`
+ADD
+    INDEX `visit_history_multi_index` (
+        `tenant_id`,
+        `competition_id`,
+        `created_at`
+    );
